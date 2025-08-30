52aa8069692876103d730be222a212f1b93695b3
Zwolake <jawadulhoq@gmail.com>
2025-08-30 20:57:00 +0600

    init

---
+++ duck_season.py
@@ -0,0 +1,36 @@
+import random
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from math import *
+#TODO - any other libraries
+
+#TODO - GameObjects
+# Duck flying
+# Duck Falling
+# Duck Landed
+# Dog(?)
+# Wolves(?)
+# Rifle
+# Bullet
+# Tree
+# Shop area
+
+#TODO - UI
+# Ammo count
+# Money display
+# Shop UI - numbers corresponding to items
+# Crosshair
+
+#TODO Implement game logic
+# Duck flying
+# Duck falling
+# Duck landed
+# Dog AI
+# Wolf AI
+# Rifle mechanics
+# Bullet mechanics
+# Border interactions
+# Shop interactions
+
+# Finishing touch
