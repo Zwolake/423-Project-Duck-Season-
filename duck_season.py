d091835e429f69132f6ccb79901966d9af7f8621
Zwolake <jawadulhoq@gmail.com>
2025-08-31 01:19:50 +0600

    rendering functions

--- duck_season.py
+++ duck_season.py
@@ -3,7 +3,9 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from math import *
+
 #TODO - any other libraries
+
 
 #TODO - GameObjects
 # Duck flying
@@ -16,16 +18,19 @@ from math import *
 # Tree
 # Shop
 
+
 #TODO - Play Area
 # Surface
 # Border
 # Shop
+
 
 #TODO - UI
 # Ammo count
 # Money display
 # Shop UI - numbers corresponding to items
 # Crosshair
+
 
 #TODO Implement game logic
 # Duck flying
@@ -38,4 +43,108 @@ from math import *
 # Border interactions
 # Shop interactions
 
-# Finishing touch
+
+#TODO Controls
+
+
+# Movement keys (WASD) + Shop menu (numbers)
+def keyboardListener(key, x, y):
+    #TODO assign movement
+
+    if key == b'w':
+        pass
+    elif key == b's':
+        pass
+    elif key == b'a':
+        pass
+    elif key == b'd':
+        pass
+
+    #TODO assign shop menu
+
+    if key == b'1':
+        pass
+    elif key == b'2':
+        pass
+    elif key == b'3':
+        pass
+    elif key == b'4':
+        pass
+
+# Look around (arrow keys)
+def specialKeyListener(key, x, y):
+    #TODO assign look around
+
+    if key == GLUT_KEY_UP:
+        pass
+    elif key == GLUT_KEY_DOWN:
+        pass
+    elif key == GLUT_KEY_LEFT:
+        pass
+    elif key == GLUT_KEY_RIGHT:
+        pass
+
+# Shoot (mouse click)
+def mouseListener(button, state, x, y):
+    #TODO assign shooting
+
+    if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
+        pass
+
+
+#TODO Finishing touch
+
+
+# camera
+def setupCamera():
+    glMatrixMode(GL_PROJECTION)  # Switch to projection matrix mode
+    glLoadIdentity()  # Reset the projection matrix
+    gluPerspective(fovY, 1.25, 0.1, 2500) # Set up a perspective projection (field of view, aspect ratio, near clip, far clip)
+    glMatrixMode(GL_MODELVIEW)  # Switch to model-view matrix mode
+    glLoadIdentity()  # Reset the model-view matrix
+
+    #TODO camera position and orientation
+
+    gluLookAt(0, 0, 0,  # Camera position
+              0, 0, 0,  # Look at point
+              0, 0, 1)  # Up vector
+    
+# display
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()  # Reset modelview matrix
+    glViewport(0, 0, 1000, 800)  # Set viewport size
+
+    #TODO setupCamera()
+
+    #TODO draw_shapes()
+
+    # Display game info text at a fixed screen position
+    # draw_text(10, 770, f"A Random Fixed Position Text")
+    # draw_text(10, 740, f"See how the position and variable change?: {enemy_body_radius}")
+
+# idle
+def idle():
+
+    #TODO update game state
+
+    glutPostRedisplay()  # Request a redraw
+
+# main loop
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)  # Double buffering, RGB color, depth test
+    glutInitWindowSize(1280, 720)  # Window size
+    glutInitWindowPosition(0, 0)  # Window position
+    duckseason = glutCreateWindow(b"Duck Season")  # Create the window
+
+    glutDisplayFunc(showScreen)  # Register display function
+    glutKeyboardFunc(keyboardListener)  # Register keyboard listener
+    glutSpecialFunc(specialKeyListener)
+    glutMouseFunc(mouseListener)
+    glutIdleFunc(idle)  # Register the idle function to move the bullet automatically
+
+    glutMainLoop()  # Enter the GLUT main loop
+
+if __name__ == "__main__":
+    main()
